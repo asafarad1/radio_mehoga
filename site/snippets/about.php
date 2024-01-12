28614e45c62a4f35ef19b3c4d17842da59eac46b
Asaf Arad <65238226+asafarad1@users.noreply.github.com>
2024-01-12 03:24:31 +0200

    gap

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -3,5 +3,5 @@
     <p><?= $page->about() ?></p>
     </div>    
     <a class="about_link" href="https://www.instagram.com/radiomehoga/" target="_blank"><?= $page->instagram() ?></a>
-    <a class="about_link" href="<?= $page->email() ?>" target="_blank"><?= $page->email() ?></a>
+    <div class="about_link"><p><?= $page->email() ?></p></div>
 </div>
