401b7ebd78acb253d60ca266502a45b0dabd2d10
Asaf Arad <asafarad1@gmail.com>
2024-11-24 01:35:25 +0200

    show date

--- site/snippets/shows.php
+++ site/snippets/shows.php
@@ -18,9 +18,7 @@
         onClick="buttonClicked(this);">
         <div class="show_tags">
             <p class="highlight"><?= $show->category() ?></p>
-        </div>
-        <div class="show_published">
-            <p class="highlight"><?= $show->published() ?></p>
+            <p class="highlight"><?= $show->published('%d.%m.%Y') ?></p>
         </div>
         <div class="show_title">
             <p class="highlight"><?= $show->show_title() ?></p>
