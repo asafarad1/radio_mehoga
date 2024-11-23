a96d68b2655beec6c97b12eb9ade8214dd636a1f
Asaf Arad <asafarad1@gmail.com>
2024-11-24 01:13:08 +0200

    bigger letter and other

--- site/snippets/shows.php
+++ site/snippets/shows.php
@@ -16,18 +16,18 @@
         style="background-image: url('<?= $show->image()->url() ?>')"
         data-variable="<?= $show->mixcloudlink() ?>"
         onClick="buttonClicked(this);">
-        <div class="show_title">
-            <p class="highlight"><?= $show->show_title() ?></p>
+        <div class="show_tags">
+            <p class="highlight"><?= $show->category() ?></p>
         </div>
         <div class="show_published">
             <p class="highlight"><?= $show->published() ?></p>
         </div>
+        <div class="show_title">
+            <p class="highlight"><?= $show->show_title() ?></p>
+        </div>
         <div class="show_info">
             <div class="show_description">
                 <p class="highlight"><?= $show->description() ?></p>
-            </div>
-            <div class="show_tags">
-                <p class="highlight"><?= $show->category() ?></p>
             </div>
         </div>
     </li>
